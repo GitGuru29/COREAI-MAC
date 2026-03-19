56a9985f467a1364b640ca05c4cbe70c8efed3ae
GitGuru29 <sdangalla66@gmail.com>
2026-03-19 09:53:28 +0530

    Initial Commit

---
+++ CoreAI Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreAI Mac
+//
+//  Created by siluna on 2026-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreAI Mac/CoreAI_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreAI_MacApp.swift
+//  CoreAI Mac
+//
+//  Created by siluna on 2026-03-19.
+//
+
+import SwiftUI
+
+@main
+struct CoreAI_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
